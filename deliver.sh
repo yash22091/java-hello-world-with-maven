d0c1f10b7b6385945513eeabb1c4e858307e951d
yash22091 <34241504+yash22091@users.noreply.github.com>
2021-12-16 13:50:59 +0530

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -5,7 +5,7 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
+mvn war:war install:install help:evaluate -Dexpression=project.name
 set +x
 
 echo 'The following complex command extracts the value of the <name/> element'
@@ -23,4 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -war target/${NAME}-${VERSION}.war
